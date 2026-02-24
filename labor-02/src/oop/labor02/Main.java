0a7db410ba01b6fca8a745e5c131bd286c8e50eb
OsztianZoltan <zoltanosztian@gmail.com>
2026-02-24 09:18:08 +0200

    labor2,feladat2

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -1,8 +1,9 @@
-package oop.labor02;
 
+package oop.labor02;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount("BT1");
+        /*BankAccount account1 = new BankAccount("BT1");
         System.out.println(account1.getAccountNumber()+ " : "+ account1.getBalance());
         account1.deposit(1000);
         System.out.println(account1.getAccountNumber()+ " : "+account1.getBalance());
@@ -16,6 +17,26 @@ public class Main {
         System.out.println(account2.getAccountNumber()+ " : "+account2.getBalance());
         account2.deposit(2000);
         System.out.println(account2.getAccountNumber()+ " : "+account2.getBalance());
+           */
+        Rectangle rectangle1 = new Rectangle(-5,0);
+        System.out.println("Length: " + rectangle1.getLength() + " Width: " + rectangle1.getWidth() + " Area: " + rectangle1.area() + " Perimeter : " + rectangle1.perimeter());
+        Rectangle[] rectangles= new Rectangle[10];
+        for(int i = 0; i < rectangles.length; i++){
+            //System.out.println(rectangles[i]);
+        }
+
+        // use a random generator
+        Random rand = new Random();
+        //generate positive random numbers less than a bound
+        double arees = 0;
+        for(int i = 0; i < rectangles.length; i++){
+            double length = 1 + rand.nextInt(10);
+            double width = 1 + rand.nextInt(10) ;
+            rectangles[ i ] = new Rectangle(length, width);
+            System.out.println("Length: " + rectangles[i].getLength() + " Width: " + rectangles[i].getWidth() + " Area: " + rectangles[i].area() + " Perimeter : " + rectangles[i].perimeter());
+            arees+=rectangles[i].area();
+        }
+        System.out.println(arees);
 
 
     }
