637d6d2f7ef197db7a3c580f39691f74842375c8
OsztianZoltan <zoltanosztian@gmail.com>
2026-02-24 08:59:29 +0200

    lab2,feladat1megoldva

--- labor-02/src/oop/labor02/Main.java
+++ labor-02/src/oop/labor02/Main.java
@@ -5,6 +5,18 @@ public class Main {
         BankAccount account1 = new BankAccount("BT1");
         System.out.println(account1.getAccountNumber()+ " : "+ account1.getBalance());
         account1.deposit(1000);
-        System.out.println(account1.withdraw(100));
+        System.out.println(account1.getAccountNumber()+ " : "+account1.getBalance());
+        account1.withdraw(500);
+        System.out.println(account1.getAccountNumber()+ " : "+account1.getBalance());
+       if( account1.withdraw(1000)) {
+           System.out.println(account1.getAccountNumber() + " : " + account1.getBalance());
+       }
+       else System.out.println("Sikertelen levetel");
+        BankAccount account2 = new BankAccount("OTP00002");
+        System.out.println(account2.getAccountNumber()+ " : "+account2.getBalance());
+        account2.deposit(2000);
+        System.out.println(account2.getAccountNumber()+ " : "+account2.getBalance());
+
+
     }
 }
